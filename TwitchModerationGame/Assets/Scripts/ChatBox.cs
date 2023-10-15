64180134cd6163bc543d1e83df01d4aba743da3b
TheLastSlice898 <56330886+TheLastSlice898@users.noreply.github.com>
2023-10-15 19:38:46 +1100

    Revert "Merge branch 'Swice' into main"
    This reverts commit e89b8db5e60db50b3811634e3c1b61813be6fde8, reversing
    changes made to 79bf4dba0d73c8963e1de9112813fc61ecab7afe.

--- TwitchModerationGame/Assets/Scripts/ChatBox.cs
+++ TwitchModerationGame/Assets/Scripts/ChatBox.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Random = UnityEngine.Random;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System;
 
 public class ChatBox : MonoBehaviour
 {
@@ -12,13 +10,10 @@ public class ChatBox : MonoBehaviour
     public TMP_InputField messageSend;
     public Button sendButton;
 
-    public static event Action OnBanned;
-    
-
-    public bool bannedUser = false; //if a person in chat has been banned
-    public string[] chatMessages = new string[6]; //where the messages will be stored
-    public int messageCount = 0; //records the number of messages
-    public string[] users = { "MeowCats12", "SmartPlant", "IlikeCheese", "BananaMan", "FriendlyGuy", "CoolDude" };
+    private bool bannedUser = false; //if a person in chat has been banned
+    private string[] chatMessages = new string[6]; //where the messages will be stored
+    private int messageCount = 0; //records the number of messages
+    private string[] users = { "MeowCats12", "SmartPlant", "IlikeCheese", "BananaMan", "FriendlyGuy", "CoolDude" };
 
     // Start is called before the first frame update
     void Start()
@@ -30,15 +25,11 @@ public class ChatBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            SendMessage();
-        }
+        
     }
 
     void SendMessage()
     {
-
         string message = messageSend.text.Trim();
 
         if (message == "")
@@ -54,9 +45,8 @@ public class ChatBox : MonoBehaviour
 
         if (message.StartsWith("!ban") && !bannedUser)
         {
-            bannedUser = false;
+            bannedUser = true;
             AddMessage("Moderator has banned the rude user.");
-            OnBanned?.Invoke();
             messageSend.text = "";
             return;
         }
